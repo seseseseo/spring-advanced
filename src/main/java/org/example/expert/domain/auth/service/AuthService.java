b634e97fb6535603a6259ffe035bc97b2d607ecf
mini-project-todo-list <leehanna0720@gmail.com>
2025-04-21 11:43:45 +0900

    :recycle: 필수과제1-1. Early Return 해결

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -25,29 +25,38 @@ public class AuthService {
 
     @Transactional
     public SignupResponse signup(SignupRequest signupRequest) {
-
-        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
-
-        UserRole userRole = UserRole.of(signupRequest.getUserRole());
-
+        //이미 존재하는 이메일일 경우 예외 발생
+        
+        /*
+          이부분 수정@!!!!!!
+        * 발생 이메일 중복 체크를 가장 먼저 수행한 후 통과한 경우에만 비밀번호를 암호화하도록 위치를 바꿈
+         */
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
             throw new InvalidRequestException("이미 존재하는 이메일입니다.");
         }
 
+        //입력받은 비밀번호를 암호화하고
+        String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+        // 입력받은 사용자 역할을 Enum으로 반환하고
+        UserRole userRole = UserRole.of(signupRequest.getUserRole());
+
+        // 새 User   객체 생성하고
         User newUser = new User(
                 signupRequest.getEmail(),
                 encodedPassword,
                 userRole
         );
+        // 레파지토리에 디비 저장
         User savedUser = userRepository.save(newUser);
-
+        // 저장된 유저 정보로 JWT 토큰을 생성한다
         String bearerToken = jwtUtil.createToken(savedUser.getId(), savedUser.getEmail(), userRole);
-
+        // 이 토큰을 열기에 넣어서 반환한다
         return new SignupResponse(bearerToken);
     }
 
     @Transactional(readOnly = true)
     public SigninResponse signin(SigninRequest signinRequest) {
+        // 이메일 기반으로 디비에서 사용자를 조회하고
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
                 () -> new InvalidRequestException("가입되지 않은 유저입니다."));
 
@@ -56,8 +65,9 @@ public class AuthService {
             throw new AuthException("잘못된 비밀번호입니다.");
         }
 
+        //인증에 성공하면 JWT 토큰 생성
         String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getUserRole());
-
+        // 로그인 성공시 토근 발급 후 응답하는 구조
         return new SigninResponse(bearerToken);
     }
 }
